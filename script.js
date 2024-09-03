eef788cfb3147e6672ca0dae1731609445c67091
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-09-03 15:36:08 +0200

    Bonus/esercizio terminato

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Daje Vue!'
+      message: 'Daje Vue!',
+      imageUrl: 'https://sm.ign.com/t/ign_it/screenshot/default/tlou-2-cover_x6nx.1024.jpg'
     }
   }
 }).mount('#app')
